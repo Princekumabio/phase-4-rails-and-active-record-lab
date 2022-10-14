31c5d8856b92dbcac33e76ffe6bcb9f3a6106334
Prince <kumabio.prince@student.moringaschool.com>
2022-10-14 06:27:40 +0000

    Completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Student.create(first_name: "Prince", last_name: "kumabio" grade: 78)
+Student.create(first_name: "Gloria", last_name: "Boateng" grade: 69)
